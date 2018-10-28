649c2f5f9305ffc32cbbd24be5cb18dc1b253296
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-28 07:25:20 +0100

    Added Automatic Deployment to S3 Webhosting Bucket

---
+++ build/precache-manifest.e7778fb635778e65e673e67a8232a5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ff708bb60429f667d58",
+    "url": "/static/js/main.4ff708bb.chunk.js"
+  },
+  {
+    "revision": "ef15c043b3c2e2b12e51",
+    "url": "/static/js/1.ef15c043.chunk.js"
+  },
+  {
+    "revision": "4ff708bb60429f667d58",
+    "url": "/static/css/main.dba56597.chunk.css"
+  },
+  {
+    "revision": "ef15c043b3c2e2b12e51",
+    "url": "/static/css/1.d673e02c.chunk.css"
+  },
+  {
+    "revision": "068b5c8f8f2d368a3453ef64d0cfee80",
+    "url": "/index.html"
+  }
+];
